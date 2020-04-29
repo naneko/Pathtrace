f82c28eed4f6627ed3f9a5ae3a4985a945d95145
Ben Saltz <bensaltz@gmail.com>
2020-04-29 15:01:28 -0600

    Updated name to match repository name

--- src/Pathtrace/Pathtrace.java
+++ src/Pathtrace/Pathtrace.java
@@ -1,12 +1,10 @@
-package Pathfinder;
+package Pathtrace;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import processing.core.PApplet;
 
-import java.util.Iterator;
-
-public class Pathfinder extends PApplet {
+public class Pathtrace extends PApplet {
     public Logger logger = LogManager.getLogger(this.getClass().getName());
 
     Path path;
@@ -23,19 +21,22 @@ public class Pathfinder extends PApplet {
     public void setup() {
         frameRate(60);
         path = new Path(width/2., height/2., Math.sin(angle), Math.cos(angle));
+//        path = new Path(50., height/2., Math.sin(angle), Math.cos(angle), 100);
         path.addQuadBound(50,50,50, height-50, height-50, height-50, height-50, 50);
+//        for(int i = 0; i < 360; i+=45) {
+//            path.addBound(sin(i)*300+width/2., cos(i)*300+height/2.,sin(i+45)*300+width/2., cos(i+45)*300+height/2.);
+//        }
+//        path.addQuadBound(50,50,50, height-100, height-50, height-77, height-153, 50);
 //        noLoop();
     }
 
     public void draw() {
         path.setStartDirection(Math.sin(angle), Math.cos(angle));
-        background(255);
+        background(0);
         iterator = path.iterator();
-        stroke(0);
-//        ellipse(mouseX, mouseY, 20, 20);
-        stroke(0, 255, 0);
+        stroke(255, 255, 0);
         path.drawBounds();
-        stroke(0);
+        stroke(255);
         while (iterator.hasNext()) {
             iterator.next();
         }
@@ -47,10 +48,30 @@ public class Pathfinder extends PApplet {
 
         angle = (angle + 0.0001) % 360;
 
-        delay(10);
+        if(terminate) {
+            stroke(255, 0, 255);
+            line(mPosX, mPosY, mouseX, mouseY);
+        }
     }
 
-//    public void mousePressed() {
+    boolean terminate = false;
+    int mPosX = 0;
+    int mPosY = 0;
+
+    public void mousePressed() {
+        if(mouseButton == LEFT) {
+            if(!terminate) {
+                mPosX = mouseX;
+                mPosY = mouseY;
+                terminate = true;
+            } else {
+                path.addBound(mPosX, mPosY, mouseX, mouseY);
+                terminate = false;
+            }
+        }
+    }
+
+    //    public void mousePressed() {
 //        setup();
 //        Sketch.debugVectors.clear();
 //        Sketch.checkLines.clear();
@@ -62,8 +83,8 @@ public class Pathfinder extends PApplet {
 //    }
 
     public static void main(String[] args){
-        String[] processingArgs = {"Pathfinder"};
-        Pathfinder pt = new Pathfinder();
+        String[] processingArgs = {"Pathtrace"};
+        Pathtrace pt = new Pathtrace();
         PApplet.runSketch(processingArgs, pt);
     }
 }
